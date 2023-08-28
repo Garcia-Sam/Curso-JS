24d2992741a692ffd57b50a15d39974e69d851ae
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-08-28 15:11:17 -0300

    Create ambiente.js

---
+++ JS/Aula13/ambiente.js
@@ -0,0 +1,22 @@
+var contador = 1
+
+do {
+    console.log(`${contador} Tudo bem?`)
+    contador++
+} while (contador <= 6)
+
+/*
+while (contador < 7){
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+}
+*/
+
+/*
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
