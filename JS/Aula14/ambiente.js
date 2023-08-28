13909bba68e34454dd0e87aeed419001b216e09f
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-08-28 15:57:19 -0300

    Create teste folder and Aula14 folder

---
+++ JS/Aula14/ambiente.js
@@ -0,0 +1,16 @@
+/* var contador = 1
+
+while(contador <= 5){
+    console.log(contador)
+    contador++
+}
+
+do{
+    console.log(contador)
+    contador++
+} while (contador <= 5)
+*/
+
+for(var contador = 1; contador <= 1000; contador++){
+    console.log(contador)
+}
