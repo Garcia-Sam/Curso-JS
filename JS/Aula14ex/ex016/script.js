8c79a3a9c4f54f3026d54e3b6a068ad10485d4c1
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-09-01 14:35:03 -0300

    update ex016

---
+++ JS/Aula14ex/ex016/script.js
@@ -0,0 +1,9 @@
+function contagem(){
+    var inicio = window.document.getElementById("numinicio")
+    var fim = window.document.getElementById("numfim")
+    var passo = window.document.getElementById("numpasso")
+
+    window.alert(inicio)
+}
+    
+
