1fda8cd284211d673bd04bbde023bfe1590181bf
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-09-01 15:51:01 -0300

    Update ex016

--- JS/Aula14ex/ex016/script.js
+++ JS/Aula14ex/ex016/script.js
@@ -1,9 +1,30 @@
 function contagem(){
-    var inicio = window.document.getElementById("numinicio")
-    var fim = window.document.getElementById("numfim")
-    var passo = window.document.getElementById("numpasso")
+    let inicio = window.document.getElementById("txtinicio")
+    let fim = window.document.getElementById("txtfim")
+    let passo = window.document.getElementById("txtpasso")
+    let res = document.getElementById("resultado")
 
-    window.alert(inicio)
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert("[ERRO] Faltam Dados!")
+    } else {
+        res.innerHTML = "Contando: "
+        let numinicio = Number(inicio.value)
+        let numfim = Number(fim.value)
+        let numpasso = Number(passo.value)
+
+        if (inicio < fim){
+            // Contagem Crescente
+            for(let c = numinicio; c <= numfim; c += numpasso) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            for (let c = numinicio; c >= numfim; c -= numpasso) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
     
 
